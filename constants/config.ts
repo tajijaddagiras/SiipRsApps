cab6bf16f4f95f2b5adc6cb9545c914c45050e00
tajijaddagira <tajijaddagirassntosa@gmail.com>
2026-04-28 01:25:15 +0700

    chore: fix expo dependency warnings and update dev api ip address

--- constants/config.ts
+++ constants/config.ts
@@ -4,7 +4,7 @@ const getBaseUrl = () => {
     if (__DEV__) {
         // Saat development, gunakan IP komputer Anda agar bisa diakses dari HP (Expo Go)
         // Silakan ganti IP ini jika IP komputer Anda berubah
-        return 'http://192.168.1.3:3000/api';
+        return 'http://192.168.1.7:3000/api';
     }
 
     if (Platform.OS === 'web') {
